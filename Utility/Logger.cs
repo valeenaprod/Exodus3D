db56faeacb7b8e733ece3cbcc0a9cae8cb943294
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-10-10 18:32:26 -0400

    Revert "Made some changes to include a Debug log"
    This reverts commit 8ea335c1ad6475af65f71ab21d6daabce40002a6.

--- Utility/Logger.cs
+++ Utility/Logger.cs
@@ -9,22 +9,25 @@ public static class Logger
 {
     public enum LogLevel { Info, Warning, Error, Debug}
 
-    private const string LogFilePath = "user://log.txt";
+    private static string _logFilePath = "user://log.txt";
 
-    public static void Log(string message, LogLevel level = LogLevel.Debug, 
+    public static void Log(string message, LogLevel level = LogLevel.Info, 
         [CallerMemberName] string memberName = "",
         [CallerFilePath] string filePath = "",
         [CallerLineNumber] int lineNumber = 0)
     {
-        if (level == LogLevel.Debug)
+        string logMessage;
+
+        if (level == LogLevel.Info)
         {
-            LogDebug(message, memberName, filePath, lineNumber);
-            return;
+            logMessage = $"{DateTime.Now}: [{level}] {message}";
         }
-        
-        var logMessage = level == LogLevel.Info
-            ? $"{DateTime.Now}: [{level}] {message}"
-            : $"{DateTime.Now}: [{level}] {message} (Caller: {memberName}, {filePath}, line {lineNumber})";
+        else
+        {
+            logMessage = $"{DateTime.Now}: [{level}] {message} " +
+                         $"(Caller: {memberName}, {filePath}, line {lineNumber}";
+        }
+            
 
         switch (level)
         {
@@ -35,25 +38,15 @@ public static class Logger
                 GD.PushWarning(logMessage);
                 break;
             case LogLevel.Info:
-                GD.Print(logMessage);
-                break;
+            case LogLevel.Debug:
             default:
                 GD.Print(logMessage);
                 break;
         }
 
         WriteToFile(logMessage);
-    }
 
-    private static void LogDebug(string message, string memberName, string filePath, int lineNumber)
-    {
-        #if DEBUG
-        var logMessage = $"{DateTime.Now}: [DEBUG] {message} " +
-                     $"(Caller: {memberName}, {filePath}, line {lineNumber}";
-        GD.Print(logMessage);
-        WriteToFile(logMessage);
-        #endif
-        
+
     }
 
     private static async void WriteToFile(string message)
@@ -62,7 +55,7 @@ public static class Logger
         {
             try
             {
-                using var logFile = FileAccess.Open(LogFilePath, FileAccess.ModeFlags.WriteRead);
+                using var logFile = FileAccess.Open(_logFilePath, FileAccess.ModeFlags.WriteRead);
                 logFile.Seek(logFile.GetLength()); // Move to end for appending
                 logFile.StoreLine(message);
             }
