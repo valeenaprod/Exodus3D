b8aee078417e0cdb6f21578c136c4b4a2569c206
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-10-10 18:59:04 -0400

    Start of some manager scripts

---
+++ Managers/ILocationManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Exodus3D.Managers;
+
+public interface ILocationManager
+{
+    void UpdateResource();
+    void TransferResourceTo(ILocationManager otherManager, string resourceType, float amount);
+}
---
+++ Managers/ResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Exodus3D.Utility;
+using Godot;
+
+namespace Exodus3D.Managers;
+
+public partial class ResourceManager : Node3D
+{
+    private Dictionary<string, ILocationManager> _locationManagers = new();
+
+    public override void _Ready()
+    {
+        Logger.Log($"_Ready called in {Name}, tree available: {GetTree() != null}");
+    }
+    public void RegisterLocation(string locationId, ILocationManager manager)
+    {
+        _locationManagers[locationId] = manager;
+    }
+
+    public void UnregisterLocation(string locationId)
+    {
+        _locationManagers.Remove(locationId);
+    }
+
+    public void TransferResource(string fromLocation, string toLocation, string resourceType, float amount)
+    {
+        if (_locationManagers.TryGetValue(fromLocation, out var from) &&
+            _locationManagers.TryGetValue(toLocation, out var to))
+        {
+            from.TransferResourceTo(to, resourceType, amount);
+        }
+    }
+    
+    
+}
