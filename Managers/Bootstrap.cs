6313b647be15142c9408955d89dcba05f63fe0a8
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-10-10 18:57:14 -0400

    GameManager & Bootstrap

--- Managers/Bootstrap.cs
+++ Managers/Bootstrap.cs
@@ -1,33 +1,65 @@
-using Exodus3D.Utility;
+using System.Diagnostics;
 using Godot;
+using Exodus3D.Utility;
 
-namespace Exodus3D.Managers;
-
-public partial class Bootstrap : Node
+namespace Exodus3D.Managers
 {
-    public override void _Ready()
+    public partial class Bootstrap : Node
     {
-        // Log Bootstrap Initialization
-        Logger.Log("Bootstrap initialized");
+        private Node _sceneContainer;
+        public static Bootstrap Instance { get; private set; }
+        public Node GameScene { get; private set; }
         
-        // Load the main menu or game scene
-        LoadInitialScene();
-    }
+        public override void _EnterTree()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                SetProcess(true);
+                Logger.Log("Bootstrap Instance Initialized...");
+            }
+            else
+            {
+                QueueFree(); 
+                Logger.Log("Bootstrap already initialized. Freeing queue.", Logger.LogLevel.Warning);
+            }
+        }
+        public override void _Ready()
+        {
+            Logger.Log("Loading Initial Scene...");
+            _sceneContainer = GetNode("SceneContainer");
+            LoadInitialScene();
+        }
 
-    private void LoadInitialScene()
+        private void LoadInitialScene()
+        {
+            const string initialScenePath = "res://Ground/GroundMap.tscn";
+            SwitchScene(initialScenePath);
+            CallDeferred(nameof(CallLoadGame));
+        }
+
+        private void CallLoadGame()
+        {
+            GameManager.Instance.LoadGame();
+        }
+        
+        public void SwitchScene(string scenePath)
+        {
+            _sceneContainer.QueueFreeChildren();
+            var newScene = GD.Load<PackedScene>(scenePath);
+            var newSceneInstance = newScene?.Instantiate();
+            _sceneContainer.AddChild(newSceneInstance);
+            GameScene = newSceneInstance;
+            Logger.Log($"Scene Switched to: {scenePath}");
+        }
+    }
+}
+
+// Helper method to free al children in the container
+public static class NodeExtensions
+{
+    public static void QueueFreeChildren(this Node node)
     {
-        // Load the initial scene (eg. Main Menu)
-        const string scenePath = "res://UI/MainMenuScene.tscn"; // Path for Main Menu Scene
-        var result = GetTree().ChangeSceneToFile(scenePath);
-
-        if (result != Error.Ok)
-        {
-            Logger.Log($"Failed to load scene: {scenePath}", Logger.LogLevel.Error);
-        }
-        else
-        {
-            Logger.Log($"Successfully loaded scene: {scenePath}");
-        }
+        foreach (var child in node.GetChildren()) child.QueueFree();
     }
-    
 }
